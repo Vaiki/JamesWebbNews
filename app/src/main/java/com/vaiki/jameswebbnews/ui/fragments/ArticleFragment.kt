9b2e08dea55ea662b1dc142257df57325df013c7
vaiki <lifeotworld@gmail.com>
2022-08-24 23:14:18 +0300

    navigation_component

---
+++ app/src/main/java/com/vaiki/jameswebbnews/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.vaiki.jameswebbnews.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.vaiki.jameswebbnews.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+}
